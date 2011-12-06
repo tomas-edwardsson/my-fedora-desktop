6d7ffc038054049e9d28b0bba23ac38c96f393a5
Tomas Edwardsson <tommi@tommi.org>
2011-12-06 20:04:37 +0000

    python-setuptools-devel has gone away

--- modules/fedorapkgr/manifests/init.pp
+++ modules/fedorapkgr/manifests/init.pp
@@ -1,5 +1,5 @@
 class fedorapkgr {
 	package { "fedora-packager": ensure => installed }
 	package { "python-devel": ensure => installed }
-	package { "python-setuptools-devel": ensure => installed }
+	package { "python-setuptools": ensure => installed }
 }
