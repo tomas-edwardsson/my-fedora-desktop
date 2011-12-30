8cd8e2805299da96deb9ca9b8d467124579e9e85
Tomas Edwardsson <tommi@tommi.org>
2011-12-30 11:50:14 +0000

    Added the C programming language

--- modules/programming/manifests/init.pp
+++ modules/programming/manifests/init.pp
@@ -2,11 +2,21 @@
 
 class programming {
 	include programming::git
+	include programming::python
+	include programming::c
 }
 
 class programming::git {
 	package { "git": ensure => installed }
 	package { "subversion": ensure => installed }
+}
+
+class programming::python {
+	# Python stuff
 	package { "ipython": ensure => installed }
 	package { "wxPython": ensure => installed }
+}
+
+class programming::c {
+	package { "gcc": ensure => installed }
 }
