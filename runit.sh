6dbd7d80fa78865c68a3d55673630676902f023e
Tomas Edwardsson <tommi@tommi.org>
2012-06-07 13:54:24 +0000

    Removed the need for Puppet directory, just runs from CWD

--- runit.sh
+++ runit.sh
@@ -3,9 +3,4 @@
 
 rpm -q puppet || sudo yum install -y puppet
 
-if [ ! -d "$HOME/Puppet" ]; then
-	echo "Couldnt find \"$HOME/Puppet\""
-	exit
-fi
-
-sudo puppet apply --modulepath $HOME/Puppet/modules -v my-fedora-desktop.pp
+sudo puppet apply --modulepath $(pwd)/modules -v my-fedora-desktop.pp
